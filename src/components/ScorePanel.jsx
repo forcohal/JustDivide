69da91c8e0cf607ec47d70c31405f2bae05e93d3
Parinav Priyadarshi <parinavpriyadarshi@gmail.com>
2026-04-20 23:10:11 +0530

    layout 2

--- src/components/ScorePanel.jsx
+++ src/components/ScorePanel.jsx
@@ -1,7 +1,7 @@
 export default function ScorePanel({ score }) {
     return (
         <div className="score-panel">
-            <h2>Score: {score}</h2>
+            <h2>Score {score}</h2>
         </div>
     );
 }
